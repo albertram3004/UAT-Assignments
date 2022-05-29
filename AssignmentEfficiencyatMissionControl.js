c5bbce851de721ffae61f4150961ccf421a9d358
alberto ramirez <alberto.ramirez@oppous.com>
2022-05-29 14:19:18 -0500

    Add files via upload

---
+++ AssignmentEfficiencyatMissionControl.js
@@ -0,0 +1,20 @@
+
+    
+    // Albert X/ 05-29-2022
+    //  loop is visible and Functional//
+    document.getElementById("countdown").innerHTML ("Assignment Efficiency at Mission Control");
+    
+    <p id="demo" onclick="countdown"></p>
+
+
+     var countdown = 5; //We defined our  veriable  as countdown
+
+     while  (countdown > 0); {
+     console.log (  countdown + "Blasstoff will iniciate in"); 
+     countdown++;
+     }
+ 
+        
+
+
+  
