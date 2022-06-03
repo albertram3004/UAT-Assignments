53f1d640859006658520b0c548d67ae2a1166cdb
alberto ramirez <alberto.ramirez@oppous.com>
2022-06-03 15:01:47 -0500

    Add files via upload

---
+++ Efficiency at Mission Control.js
@@ -0,0 +1,24 @@
+
+    
+    // Albert X/ 05-27-2022/Great assignment
+    // Learning still Lol, but our loop is visible and Functional//
+    document.getElementById("countdown").innerHTML ("Efficiency at Mission Control");
+
+
+     var countdown = 5; //We defined our  veriable  as countdown
+
+     while  (countdown > 0); {
+     console.log (  countdown + "Blasstoff will iniciate in"); 
+     countdown++;
+     }
+ 
+     
+
+     
+
+      
+
+
+
+
+    
